b38675c22c8fe1d6e89df0fe42c4ffe35c8e5f20
James Robertson <54756070+McJimble@users.noreply.github.com>
2023-07-14 17:37:07 -0500

    First att. adding playlist compatibility
    Probably doesn't work but it could. Can't test it fully right now as I'm pushing this since I'm working from a different computer and forgot to push some changes from main dev machine lol.

--- jotbot/cogs/music_and_sfx/music_and_sfx.py
+++ jotbot/cogs/music_and_sfx/music_and_sfx.py
@@ -30,7 +30,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         'audioformat': 'mp3',
         'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
         'restrictfilenames': True,
-        'noplaylist': True,
+        'yesplaylist': True,
         'nocheckcertificate': True,
         'ignoreerrors': False,
         'logtostderr': False,
@@ -72,47 +72,68 @@ class YTDLSource(discord.PCMVolumeTransformer):
     def __str__(self):
         return '**{0.title}** by **{0.uploader}**'.format(self)
 
-    @classmethod
-    async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
+    # Returns a list of YTDL sources for the given search. For single videos, the list will have one sources.
+    # For playlists, each source from the playlist will be included in the list.
+    async def create_sources(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None) -> list: 
+        
+        # Processes a single entry and returns the YTDLSource.
+        async def process_entry(loop: asyncio.BaseEventLoop, entry: str):
+            webpage_url = entry['webpage_url']
+            partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
+            processed_info = await loop.run_in_executor(None, partial)
+
+            print(processed_info)
+
+            if processed_info is None:
+                raise Exception('Couldn\'t fetch `{}`'.format(webpage_url))
+
+            if 'entries' not in processed_info:
+                info = processed_info
+            else:
+                info = None
+                while info is None:
+                    try:
+                        info = processed_info['entries'].pop(0)
+                    except IndexError:
+                        raise Exception('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
+
+            return YTDLSource(ctx, discord.FFmpegPCMAudio(info['url'], **cls.FFMPEG_OPTIONS), data=info)
+        
         loop = loop or asyncio.get_event_loop()
 
         partial = functools.partial(cls.ytdl.extract_info, search, download=False, process=False)
         data = await loop.run_in_executor(None, partial)
         data = yt_dlp.YoutubeDL.sanitize_info(data)
 
+        print(data)
+
         if data is None:
             raise Exception('Couldn\'t find anything that matches `{}`'.format(search))
 
+        sources = []
+
+        # Single video; no 'entries'; just the one video data.
         if 'entries' not in data:
-            process_info = data
+            result = process_entry(data)
+            if result is None:
+                raise Exception('Error processing entry `{}`'.format(data))
+            else:
+                sources.append(result)
+
+        # Playlist; go through each entry and add to list.
         else:
-            process_info = None
+            entryProcessed = False
             for entry in data['entries']:
                 if entry:
-                    process_info = entry
-                    break
-
-            if process_info is None:
-                raise Exception('Couldn\'t find anything that matches `{}`'.format(search))
-
-        webpage_url = process_info['webpage_url']
-        partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
-        processed_info = await loop.run_in_executor(None, partial)
-
-        if processed_info is None:
-            raise Exception('Couldn\'t fetch `{}`'.format(webpage_url))
-
-        if 'entries' not in processed_info:
-            info = processed_info
-        else:
-            info = None
-            while info is None:
-                try:
-                    info = processed_info['entries'].pop(0)
-                except IndexError:
-                    raise Exception('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
-
-        return cls(ctx, discord.FFmpegPCMAudio(info['url'], **cls.FFMPEG_OPTIONS), data=info)
+                    result = process_entry(entry)
+                    if result is not None:
+                        entryProcessed = True
+                        sources.append(result)
+            
+            if not entryProcessed:
+                raise Exception('Error processing entry `{}`'.format(entry))
+        
+        return sources
 
     @staticmethod
     def parse_duration(duration: int):
@@ -433,15 +454,24 @@ class MusicSFXCog(ServerOnlyCog, name = "Music/Audio"):
 
         async with ctx.typing():
             try:
-                source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop)
+                sources = await YTDLSource.create_sources(ctx, search, loop=self.bot.loop)
             except Exception as e:
                 await ctx.send('An error occured processing this request: {}'.format(str(e)))
             else:
-                song = Song(source)
-
-                await self.get_voice_state(ctx).musicQueue.put(song)
-                await ctx.send('{} added to queue.'.format(str(source)))
-
+                if len(sources) == 0:
+                    await ctx.send('Error: URL returned no sources')
+                elif len(sources) == 1:
+                    song = Song(sources[0])
+                    await self.get_voice_state(ctx).musicQueue.put(song)
+                    await ctx.send('{} added to queue.'.format(str(source)))
+                else:
+                    cnt = 0
+                    for source in sources:
+                        song = Song(sources[0])
+                        await self.get_voice_state(ctx).musicQueue.put(song)
+                        cnt += 1
+                    await ctx.send('Successfully added {} videos from playlist.'.format(str(cnt)))
+    
     @commands.command(name='VoteSkip', aliases=['vs'], help='Same as !skip, but guarantees a vote is started (instead of being bypassed by admins/requester). **Shorthand: !vs**')
     async def voteskip(self, ctx: commands.Context):
         await self.skip(ctx, adminVote='v')
@@ -635,7 +665,6 @@ class MusicSFXCog(ServerOnlyCog, name = "Music/Audio"):
 
     @commands.command(name='Disconnect', aliases=['dc'], help='Disconnects the bot from the current voice channel. **Shorthand: !dc**')
     async def disconnect(self, ctx: commands.Context):
-
         state = self.get_voice_state(ctx)
         if state and state.voice:
             if ctx.author.voice.channel != self.get_voice_state(ctx).voice.channel:
